a52c21b548345588e320a37e115a23b30699b6e0
Miguel Reyes <miguel.reyes@enernoc.com>
2018-04-26 17:03:09 -0400

    Web app testing with supertest library and expect.

--- server/server.js
+++ server/server.js
@@ -4,8 +4,20 @@ var app = express();
 
 app.get('/', (req, res) => {
   res.status(404).send({
-    error: 'Page not found.'
+    error: 'Page not found.',
+    name : 'Todo App v1.0'
   });
+});
+
+app.get('/users', (req, res) => {
+  res.status(200).send([{
+    name: 'Miguel',
+    age : 37
+  }, {
+    name: 'Angel',
+    age: 27
+  }
+  ]);
 });
 
 app.listen(3000);
--- server/server.test.js
+++ server/server.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server').app;
 
@@ -6,8 +7,24 @@ it('should return Hello World response', (done) => {
   request(app)
     .get('/')
     .expect(404)
-    .expect({
-      error: 'Page not found.'
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.'
+      });
+    })
+    .end(done);
+});
+
+
+it('should my user object', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name: 'Miguel',
+        age: 37
+      });
     })
     .end(done);
 });
