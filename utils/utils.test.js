1aa48a441b5e95dae021e471a698b7f40a824a43
Miguel Reyes <miguel.reyes@enernoc.com>
2018-04-26 14:41:12 -0400

    Testing async functions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,6 +8,13 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should asyncAdd two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('should square number', () => {
   var res = utils.square(2);
 
